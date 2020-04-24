39a0fd553a09c91c5fbf86b2d56177fa4cf45559
Vishva Patel <vnspvt.123@gmail.com>
2020-04-24 22:17:17 +0530

    Exception Handeling Program
    Find Confedential.txt file from 'master' branch and then run the program.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,34 @@
 ﻿using System;
+using System.IO; //mandatory header file to perform IO operations.
 
-namespace Constructor_overloading
+namespace Exceptionhandeling
 {
     class Program
     {
-        //Class to evaluate the cost of pencil
-        public class Cost
-        {
-            private double Amount; //The amoount of one item 
-            private int Qty; //The quamtity required
-            public Cost(double amt, int qty)
-            {
-                Amount = amt;
-                Qty = qty;
-            }
-
-            public Cost(int qty)
-            {
-                Amount = 0.5;
-                Qty = qty;
-            }
-
-            public double Calc()
-            {
-                return this.Amount * this.Qty; //Formula to calculate the price.    
-            }
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a program for constructor overloading");
-            Cost c = new Cost(12);
-            double price = c.Calc();
-            Console.WriteLine("The total price to pay is Rs.{0}", price);
+            StreamReader rdfile = null; //Object that reads the content of the file.
+            try //try catch is required to handle the unknown wxception ehilr reading and excwessing the file.
+            {
+                rdfile = File.OpenText("../../../Confedential.txt"); //passing th efile location to be opened
+                Console.WriteLine(rdfile.ReadToEnd()); //O/p all the contents of the file.
+            }
+            catch (FileNotFoundException nf) //Specific exception this block handles the exception.
+            {
+                Console.WriteLine(nf.Message);
+            }
+            catch(Exception ex) //if don't know the exact exception then can use general object Exception to handle the exception.
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //Anything in this block is run even if exception occurs or not.
+            {
+                if (rdfile != null)
+                {
+                    rdfile.Close(); //Closes the object thus, stops reading the file.
+                    Console.WriteLine("object closed");
+                }
+            }
         }
-
     }
 }
