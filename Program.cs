19a49c4b4386812839eddb9db1115773a0141786
Vishva Patel <vnspvt.123@gmail.com>
2020-04-24 22:15:48 +0530

    Constructor Overloading Program

--- Program.cs
+++ Program.cs
@@ -1,85 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
-namespace Solution
+namespace Constructor_overloading
 {
-    class student
+    class Program
     {
-        int id;
-        string name;
-        string course;
-        int mark;
-        public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
-        public int Mark { get => mark; set => mark = value; }
-        public string Course { get => course; set => course = value; }
+        //Class to evaluate the cost of pencil
+        public class Cost
+        {
+            private double Amount; //The amoount of one item 
+            private int Qty; //The quamtity required
+            public Cost(double amt, int qty)
+            {
+                Amount = amt;
+                Qty = qty;
+            }
 
-        //public student(int id,string name, int mark, string course)
-        /*{
-            this.Id = id;
-            this.Name = name;
-            this.Mark = mark;
-            this.Course = course;
-        }*/
+            public Cost(int qty)
+            {
+                Amount = 0.5;
+                Qty = qty;
+            }
 
-    }
-
-    class Solution
-    {
+            public double Calc()
+            {
+                return this.Amount * this.Qty; //Formula to calculate the price.    
+            }
+        }
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            student s = new student();
-           List<string> sobj = new List<string>(4);
-           List<string> stud = new List<string>();
-            for (int i = 1; i <= n; i++)
-            {
-                try
-                {
-                    Console.Write($"Enter the ID of the {i}th student");
-                    s.Id = Convert.ToInt32(Console.ReadLine());
-                    sobj.Append(s.Id.ToString());
-                    Console.Write($"Enter the name of the {i}th student");
-                    s.Name = Console.ReadLine();
-                    sobj.Append(s.Name);
-                    Console.Write($"Enter the mark of the {i}th student");
-                    s.Mark = Convert.ToInt32(Console.ReadLine());
-                    sobj.Append(s.Mark.ToString());
-                    Console.Write($"Enter the course of the {i}th student");
-                    s.Course = Console.ReadLine();
-                    sobj.Append(s.Course);
-                    stud.Add(sobj.ToString());
-                    sobj.Clear();
-                }
-                catch(Exception E)
-                {
-                    Console.WriteLine(E.Message);
-                }
-            }
-            Console.Write("Enter the course to find");
-            string course1 = Console.ReadLine();     
-            Console.WriteLine($"Max marks in {course1} is of {showmax(stud, course1)}");   
+            Console.WriteLine("This is a program for constructor overloading");
+            Cost c = new Cost(12);
+            double price = c.Calc();
+            Console.WriteLine("The total price to pay is Rs.{0}", price);
         }
-        public static string showmax(List<string> stud, string course)
-        {
-           
-            int maxmks = 0;
-            string name = string.Empty;
-            foreach(string s in stud)
-            {
-                if(s.Course == course)
-                {
-                    if(s.Mark > maxmks)
-                    {
-                        maxmks = s.Mark;
-                        name = s.Name;
-                    }
-                }
 
-            }
-            return name;
-        }
     }
 }
