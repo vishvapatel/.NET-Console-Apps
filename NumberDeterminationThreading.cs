0653d85b16540179e0fbf31fc441969a5ded47a9
Vishva Patel <vnspvt.123@gmail.com>
2020-05-05 15:47:16 +0530

    Multi Threading in C# Problems

---
+++ NumberDeterminationThreading.cs
@@ -0,0 +1,82 @@
+﻿/*
+ Author: Vishva Patel
+ Github: .NET-Console-Apps 
+ Program is based on MultiThreading in C#.
+
+Question 2: Write a program which first generates a set of random numbers and then determines negative, positive even, positive odd numbers concurrently.
+ 
+ */
+
+using System;
+using System.Threading;
+
+namespace NumberDetermination
+{
+    class Numbers
+    {
+        private int[] arr = new int[10]; //Creating an array to generate numbers
+
+        readonly Random r = new Random(); //Initializing Random class for random numbers.
+
+        //This constructor intialzes array arr with random number from -100 to +100.
+        public Numbers()
+        {
+            for(int i=0; i<10; i++)
+            {
+                
+                arr[i] = r.Next(-100, 100);
+            }
+        }
+        //This method is responsible fro checking whether the number is positive or negative.
+        public void CheckSign()
+        {
+            foreach (int i in arr)
+            {
+                if(i < 0)
+                    Console.WriteLine($"{i} is negative"); 
+                else
+                    Console.WriteLine($"{i} is Positive");
+            }
+        }
+        //This method is responsible to determine whether the number is even or odd.
+        public void CheckEvenOdd()
+        {
+           
+            foreach (int i in arr)
+            {
+                if (i > 0) //Check only positive Even and Odd
+                {
+                    if (i % 2 == 0)
+                        Console.WriteLine($"{i} is Even");
+                    else
+                        Console.WriteLine($"{i} is odd");
+                }
+            }
+            
+        }
+
+    }
+    class NumberDeterminationThreading
+    {
+        static void Main(string[] args)
+        {
+
+            Numbers num = new Numbers(); //Initializing the class.
+            //Assigned methods to threads.
+            Thread sign = new Thread(num.CheckSign); 
+            Thread EvenOdd = new Thread(num.CheckEvenOdd); 
+
+            try
+            {
+                //Starting thread.
+                sign.Start();
+                EvenOdd.Start();
+            }catch(Exception e)
+            {
+                //Destroying thread.
+                sign.Abort();
+                EvenOdd.Abort();
+            }
+        }
+    }
+}
---
+++ StackSimultaneous.cs
@@ -0,0 +1,61 @@
+﻿/*
+ Author: Vishva Patel
+ Github: .NET-Console-Apps 
+ Program is based on MultiThreading in C#.
+
+Question 1: Write a program which handles Push operation and Pop operation on stack concurrently.
+ 
+ */
+
+
+using System;
+using System.Collections;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Stck
+    {
+       public Stack s = new Stack();
+        public void PushElement()
+        {
+
+            s.Push(4);
+            s.Push(5);
+            Console.WriteLine($"Stack Push");
+        }
+        public void PopElement()
+        {
+            if (s.Count != 0)
+            {
+                s.Pop();
+                Console.WriteLine($"Popped {s.Peek()} from stack");
+            }
+            
+        }
+    }
+    class StackSimultaneous
+    { 
+        //There are total three threads created PushThread PopThread and the Main Thread.
+        static void Main(string[] args)
+        {
+            Stck st = new Stck();
+            Thread PushThread = new Thread(new ThreadStart(st.PushElement)); //There are two methods to initialize the thread using this ThreadStart delegate.
+            Thread PopThread = new Thread(st.PopElement); //Another using this class instance method.
+            try
+            {
+               
+                PushThread.Start(); //Starting Thread
+                PopThread.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(PushThread.Name + "Ended");
+                PushThread.Abort(); //Destroying Thread
+                Console.WriteLine(PopThread.Name + "Ended");
+                PopThread.Abort(); //Destroying Thread.
+            }
+            Console.WriteLine("Main thread Ended"); //Main thread ends and program execution stops and all the cildren thread are auto destroyed.
+        }
+    }
+}
